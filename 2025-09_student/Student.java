5ff0d0c9587c857bbcd74c8cf43b22c6a0fd392d
Georg Graf (SPG) <grafg@spengergasse.at>
2025-09-18 17:59:59 +0200

    acp-commit

--- 2025-09_student/Student.java
+++ 2025-09_student/Student.java
@@ -7,17 +7,19 @@ public class Student
 
     // Constructor
     // name: mind. 3 char, max 50
-    public Student (String name, float kg, int cm) {
+    public Student (String name, float kg, int cm, char gender) {
         this.setName(name);
         this.setKg(kg);
         this.setCm(cm);
+        this.setGender(gender);
     }
     // setter für name
     public void setName (String name) {
         // TODO separate Meldungen zu kurz bzw. zu lang
-        if (name.length() < 3 || name.length() > 50) {
+        if ((name.length() < 3) || (name.length() > 50)) {
             throw new IllegalArgumentException("Name muss 3-50 Buchstaben haben");
         }
+        // Ab hier weiss ich dass name meinen Kriterien entspricht
         this.name = name;
     }
 
@@ -30,7 +32,27 @@ public class Student
     public void setCm (int cm) {
         this.cm = cm;
     }
+    // SETTer gender
+    public void setGender (char g) {
+        // akzeptiere hier nur 'm' oder 'f' sowie 'M' oder 'F'
+        g = Character.toLowerCase(g);
+        // jetzt ist g mit Sicherheit klein
+        if ((g != 'm') && (g != 'f')) {
+            throw new IllegalArgumentException("Gender darf nur m oder f sein. Ich nammat sogar Großbuchstaben.");            
+        }
+        this.gender = g;
+    }
 
+    public void setGenderBal (char g) {  // TEst auf GUT
+        // akzeptiere hier nur 'm' oder 'f' sowie 'M' oder 'F'
+        g = Character.toLowerCase(g);
+        // jetzt ist g mit Sicherheit klein
+        if (g == 'f' || g == 'm') {  // also gültig
+            this.gender = g;
+            return;
+        } 
+        throw new IllegalArgumentException("Gender darf nur m oder f sein. Ich nammat sogar Großbuchstaben.");
+    }
     // calculate bmi
     public float bmi () {
         return this.kg/((this.cm/100.0f)*(this.cm/100.0f));
